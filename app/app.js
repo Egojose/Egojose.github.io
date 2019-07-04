af49ba4c5b8948ee5521ef0a93344edfa72d9089
Egojose <josemicrobiologo@gmail.com>
2019-07-04 18:43:13 -0500

    funcion para validar numero de usuario

--- app/app.js
+++ app/app.js
@@ -7,3 +7,17 @@ function shuffleArray(array) { //generate randm numbers
     };
     return array.slice(0, 4).join('');
 };
+
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var secret_number = shuffleArray(numbers);
+console.log( secret_number ); //Cada que se cargue la pag aparece un num random
+
+function validate(new_number, marray) {//entra el mnumero del user y lo revisa
+    var isnum = /^\d+$/.test(new_number); //comprueba q lo escrito sean numeros
+    if ((new_number.length != 4) || (marray[0] === marray[1] || marray[1] === marray[2] || marray[2] === marray[3] || marray[3] === marray[4]) || (isnum === false)) {
+        $('.error').css("color", "red");
+    } else {
+        $('.error').css("color", "white");
+        haveFourDigits(new_number, secret_number);
+    };
+};
